8b2e205c0566e07bc247678c47af156523a28588
KLINY1337 <semyon.chernomurov@gmail.com>
2023-12-23 18:59:05 +0300

    Initial commit (configured gradle.build)

---
+++ src/test/java/com/test/task/sputnik/currencyexchangeapi/TestCurrencyExchangeApiApplication.java
@@ -0,0 +1,23 @@
+package com.test.task.sputnik.currencyexchangeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestCurrencyExchangeApiApplication {
+
+	@Bean
+	@ServiceConnection
+	PostgreSQLContainer<?> postgresContainer() {
+		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.from(CurrencyExchangeApiApplication::main).with(TestCurrencyExchangeApiApplication.class).run(args);
+	}
+
+}
