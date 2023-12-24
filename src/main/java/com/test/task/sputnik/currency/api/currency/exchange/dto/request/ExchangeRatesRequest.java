b0b999d748e8e237d5db79b84fe4d73b0c25406b
KLINY1337 <semyon.chernomurov@gmail.com>
2023-12-24 14:59:04 +0300

    created aop logging

--- src/main/java/com/test/task/sputnik/currency/api/currency/exchange/dto/request/ExchangeRatesRequest.java
+++ src/main/java/com/test/task/sputnik/currency/api/currency/exchange/dto/request/ExchangeRatesRequest.java
@@ -1,11 +1,23 @@
 package com.test.task.sputnik.currency.api.currency.exchange.dto.request;
 
 import com.test.task.sputnik.currency.api.currency.exchange.enumeration.Currency;
+import com.test.task.sputnik.currency.api.currency.exchange.enumeration.validator.EnumValidator;
 import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.validation.annotation.Validated;
 
 import java.io.Serializable;
 import java.util.List;
 import java.util.Set;
 
-public record ExchangeRatesRequest(Currency sourceCurrency, Set<Currency> destinationCurrencies) implements Serializable {
+@Validated
+public record ExchangeRatesRequest(
+
+        @NotNull(message = "Source currency is not specified")
+        @EnumValidator(targetClassType = Currency.class, message = "Unacceptable source currency specified")
+        Currency sourceCurrency,
+
+        @Nullable
+        @EnumValidator(targetClassType = Currency.class, message = "Unacceptable destination currency specified")
+        Set<Currency> destinationCurrencies) implements Serializable {
 }
